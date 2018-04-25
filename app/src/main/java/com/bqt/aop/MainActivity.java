4adec6da3a50fcba51f1cf745fbc1f5537bfa2e8
baiqiantao <baiqiantao@sina.com>
2018-04-25 18:42:51 +0800

    可以正常跑起来了

--- app/src/main/java/com/bqt/aop/MainActivity.java
+++ app/src/main/java/com/bqt/aop/MainActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.bqt.aop.aspectj.TestAnnoTrace;
+import com.bqt.aop.test.BqtKeyLog;
+import com.bqt.aop.test.BqtLog;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -22,6 +26,9 @@ public class MainActivity extends ListActivity {
 		setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>(Arrays.asList(array))));
 	}
 	
+	@BqtLog
+	@BqtKeyLog("包青天")
+	@TestAnnoTrace("包青天")
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		switch (position) {
